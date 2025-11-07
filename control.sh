7e84bc3c7c3e8c025a0d9cd15bede16ee637d422
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-11-07 11:39:38 -0500

    Don't use deno allow all

--- control.sh
+++ control.sh
@@ -1,12 +1,12 @@
 if [ $1 == "register" ]
 then
     set -e
-    deno run --allow-all --check ./deploy.ts
+    deno run --allow-env --allow-ffi --allow-net --allow-read=commands --check ./deploy.ts
 fi
 if [ $1 == "dev" ]
 then
     set -e
-    deno run --allow-all --check ./index.ts
+    deno run --allow-env --allow-ffi --allow-net --allow-read=commands,node_modules,utils --allow-run=ffmpeg,yt-dlp --check ./index.ts
 fi
 if [ $1 == "start" ]
 then
@@ -15,7 +15,7 @@ then
     pm2 delete "discordbot"
     set -e
     pm2 save --force
-    pm2 start ./index.ts --interpreter="deno" --interpreter-args="run --allow-all --check" --name "discordbot"
+    pm2 start ./index.ts --interpreter="deno" --interpreter-args="run --allow-env --allow-ffi --allow-net --allow-read=commands,node_modules,utils --allow-run=ffmpeg,yt-dlp --check" --name "discordbot"
     pm2 save --force
     echo "Successfully started application process."
 fi
