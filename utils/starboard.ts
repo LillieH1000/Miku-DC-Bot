4fa027d1c73920a45ef94fb06327a64b8aa98de4
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-11-14 15:47:01 -0500

    Fix starboard showing with every new star reaction

--- utils/starboard.ts
+++ utils/starboard.ts
@@ -3,11 +3,16 @@ import { Client, TextChannel } from "discord.js"
 function invoke(client: Client) {
     client.on("messageReactionAdd", async (reaction, member) => {
         if (member.bot || !reaction.message.guild) return
-    
+        
         // Legacy Update
-        if (reaction.message.guild.id == "1095995920409178112" && reaction.emoji.name == "⭐") {
+        if (reaction.message.guild.id != "1095995920409178112") {
+            return
+        }
+    
+        if (reaction.emoji.name == "⭐" && reaction.message.reactions.cache.get("⭐")?.count == 1) {
             const channel = reaction.message.guild.channels.cache.get("1318066740940242954") as (TextChannel | undefined)
             if (!channel) return
+
             await channel.send("⭐")
             await reaction.message.forward(channel)
         }
