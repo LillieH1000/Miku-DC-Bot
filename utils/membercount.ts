467ae0ecddfc5af5b26cf0a52de78816c3c98272
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-11-20 16:21:45 -0500

    Add legacy update to members count

--- utils/membercount.ts
+++ utils/membercount.ts
@@ -1,11 +1,16 @@
 import { Client, Guild, TextChannel } from "discord.js"
 
 function invoke(client: Client) {
-    // Town Of Salem Server
     setInterval(async function() {
-        const guild = client.guilds.cache.get("416350699794857986") as (Guild | undefined)
-        const channel = guild?.channels.cache.get("772878081907884072") as (TextChannel | undefined)
-        await channel?.setName(`Members: ${guild?.memberCount}`)
+        // Crowbar World
+        const guild1 = client.guilds.cache.get("416350699794857986") as (Guild | undefined)
+        const channel1 = guild1?.channels.cache.get("772878081907884072") as (TextChannel | undefined)
+        await channel1?.setName(`Members: ${guild1?.memberCount}`)
+
+        // Legacy Update
+        const guild2 = client.guilds.cache.get("1095995920409178112") as (Guild | undefined)
+        const channel2 = guild2?.channels.cache.get("1441175481641865428") as (TextChannel | undefined)
+        await channel2?.setName(`Members: ${guild2?.memberCount}`)
     }, 60000)
 }
 
