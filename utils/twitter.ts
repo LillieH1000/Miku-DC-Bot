2821930da85d7d69e015460d3c476c93498ccc54
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-12-22 17:44:21 -0500

    Add fxtwitter and vxtwitter override to util

--- utils/twitter.ts
+++ utils/twitter.ts
@@ -25,58 +25,59 @@ function invoke(client: Client) {
         const messageContent: string = message.content.replace(/(\r|\n|\r\n|<|>)/gm, " ")
     
         for (const word of messageContent.split(" ")) {
-            if ((word.match(/^http(?:s)?:\/\/(.*)twitter\.com\//) || word.match(/^http(?:s)?:\/\/x\.com\//)) && !word.match(/^http(?:s)?:\/\/(.*)fxtwitter\.com\//) && !word.match(/^http(?:s)?:\/\/(.*)vxtwitter\.com\//)) {
-                const res: Response = await fetch(word.replace(/twitter.com/gm, "api.fxtwitter.com").replace(/x.com/gm, "api.fxtwitter.com"))
-                if (!res.ok) continue
-                const data: resData = await res.json()
-                
-                const container: ContainerBuilder = new ContainerBuilder()
-                    .setAccentColor(+deno.keys.accent)
+            const rx: RegExp = /^(.*?)(?:fxtwitter|twitter|vxtwitter|x)\.com(.*)/gm
+            if (!word.match(rx)) continue
 
-                const section: SectionBuilder = new SectionBuilder()
-                    .addTextDisplayComponents(
-                        new TextDisplayBuilder()
-                            .setContent(bold(`${data.tweet.author.name} (${data.tweet.author.screen_name})`)),
-                    )
-                    .setThumbnailAccessory(
-                        new ThumbnailBuilder()
-                            .setURL(data.tweet.author.avatar_url)
-                    )
+            const res: Response = await fetch(word.replace(rx, "$1api.fxtwitter.com$2"))
+            if (!res.ok) continue
+            const data: resData = await res.json()
+            
+            const container: ContainerBuilder = new ContainerBuilder()
+                .setAccentColor(+deno.keys.accent)
 
-                if (data.tweet.text != "") {
-                    section.addTextDisplayComponents(
-                        new TextDisplayBuilder()
-                            .setContent(data.tweet.text),
-                    )
-                }
-
-                container.addSectionComponents(section).addSeparatorComponents(
-                    new SeparatorBuilder()
+            const section: SectionBuilder = new SectionBuilder()
+                .addTextDisplayComponents(
+                    new TextDisplayBuilder()
+                        .setContent(bold(`${data.tweet.author.name} (${data.tweet.author.screen_name})`)),
+                )
+                .setThumbnailAccessory(
+                    new ThumbnailBuilder()
+                        .setURL(data.tweet.author.avatar_url)
                 )
 
-                if (data.tweet.media) {
-                    const gallery: MediaGalleryBuilder = new MediaGalleryBuilder()
-
-                    data.tweet.media.all.forEach(media => {
-                        gallery.addItems(new MediaGalleryItemBuilder().setURL(media.url))
-                    })
-
-                    container.addMediaGalleryComponents(gallery).addSeparatorComponents(
-                        new SeparatorBuilder()
-                    )
-                }
-
-                container.addActionRowComponents(new ActionRowBuilder<ButtonBuilder>()
-                    .addComponents(
-                        new ButtonBuilder()
-                            .setLabel("Open Tweet")
-                            .setStyle(ButtonStyle.Link)
-                            .setURL(data.tweet.url)
-                    ))
-
-                await message.suppressEmbeds(true)
-                await message.reply({ components: [container], flags: MessageFlags.IsComponentsV2 })
+            if (data.tweet.text != "") {
+                section.addTextDisplayComponents(
+                    new TextDisplayBuilder()
+                        .setContent(data.tweet.text),
+                )
             }
+
+            container.addSectionComponents(section).addSeparatorComponents(
+                new SeparatorBuilder()
+            )
+
+            if (data.tweet.media) {
+                const gallery: MediaGalleryBuilder = new MediaGalleryBuilder()
+
+                data.tweet.media.all.forEach(media => {
+                    gallery.addItems(new MediaGalleryItemBuilder().setURL(media.url))
+                })
+
+                container.addMediaGalleryComponents(gallery).addSeparatorComponents(
+                    new SeparatorBuilder()
+                )
+            }
+
+            container.addActionRowComponents(new ActionRowBuilder<ButtonBuilder>()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setLabel("Open Tweet")
+                        .setStyle(ButtonStyle.Link)
+                        .setURL(data.tweet.url)
+                ))
+
+            await message.suppressEmbeds(true)
+            await message.reply({ components: [container], flags: MessageFlags.IsComponentsV2 })
         }
     })
 }
