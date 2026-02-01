f75c2e2e80f6077a109fed08513298c6f43fe6ba
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-01-31 20:36:35 -0500

    Revert "Remove old miku statuses"
    This reverts commit 911ed5de6b6bb5955cc5948be5354a349f5552e6.

---
+++ utils/status.ts
@@ -0,0 +1,28 @@
+import { ActivityType, Client } from "discord.js"
+import { TZDate } from "@date-fns/tz"
+import { format } from "date-fns"
+
+let hour: number
+
+function invoke(client: Client) {
+    setInterval(function() {
+        if (client.user) {
+            const date: TZDate = TZDate.tz("America/New_York")
+            const currentdate: number = +format(date, "e")
+            const currenthour: number = +format(date, "h")
+            if (currentdate == 2) {
+                hour = -1
+                client.user.setActivity("It's Miku monday", { type: ActivityType.Custom })
+            } else {
+                if (hour != currenthour) {
+                    hour = currenthour
+                    const statuses: string[] = ["Miku gang", "oo ee oo", "I'm thinking Miku", "Hits u with a pipe"]
+                    const status: string = statuses[Math.floor(Math.random() * statuses.length)]
+                    client.user.setActivity(status, { type: ActivityType.Custom })
+                }
+            }
+        }
+    }, 60000)
+}
+
+export { invoke }
