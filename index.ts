462d13fcfe047cad3bcf735e5f9209be96622d58
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-01-31 22:20:34 -0500

    Add update prompt on start, commented out

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, Client, Collection, Events, GatewayIntentBits, SlashCommandBuilder } from "discord.js"
+import { bold, ChatInputCommandInteraction, Client, Collection, ContainerBuilder, Events, GatewayIntentBits, Guild, MessageFlags, SectionBuilder, SeparatorBuilder, SlashCommandBuilder, TextChannel, TextDisplayBuilder, ThumbnailBuilder } from "discord.js"
 import fs from "node:fs"
 
 interface clientCollection extends Client {
@@ -55,6 +55,44 @@ for (const file of utilsFiles) {
 
 client.once(Events.ClientReady, () => {
 	console.log(`Logged in as ${client.user!.tag}.`)
+
+	// Bot Updates
+	
+	const container = new ContainerBuilder()
+        .setAccentColor(+Deno.env.get("ACCENT")!)
+		.addSectionComponents(
+            new SectionBuilder()
+				.addTextDisplayComponents(
+					new TextDisplayBuilder()
+						.setContent(bold("Hey everybody Miku here!")),
+					new TextDisplayBuilder()
+						.setContent("Just wanted to let you all know some new changes to me!")
+				)
+				.setThumbnailAccessory(
+					new ThumbnailBuilder()
+						.setURL(client.user!.displayAvatarURL())
+				)
+		)
+		.addSeparatorComponents(
+			new SeparatorBuilder()
+		)
+		.addTextDisplayComponents(
+			new TextDisplayBuilder()
+				.setContent("- I now support music using /play, you can either enter the song name or use soundcloud and bandcamp directly."),
+			new TextDisplayBuilder()
+				.setContent("- I am now completely open source: https://github.com/LillieH1000/Miku-DC-Bot."),
+			new TextDisplayBuilder()
+				.setContent("- I am now a completely public bot and free to use: https://discord.com/api/oauth2/authorize?client_id=1065377660303310859&permissions=8&scope=bot%20applications.commands.")
+		)
+
+	// Dev Server
+	/* const guild: Guild | undefined = client.guilds.cache.get("1128424035173273620") as (Guild | undefined)
+	const channel: TextChannel | undefined = guild?.channels.cache.get("1440059965925494804") as (TextChannel | undefined)
+    channel?.send({ components: [container], flags: MessageFlags.IsComponentsV2 }) */
+	// Default Channel
+	/* client.guilds.cache.forEach(guild => {
+		guild.systemChannel?.send({ components: [container], flags: MessageFlags.IsComponentsV2 })
+	}) */
 })
 
 client.login(Deno.env.get("TOKEN"))
