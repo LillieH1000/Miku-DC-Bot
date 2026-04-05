d4ac899c0615211b3509d2d746220ba26087334a
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-04-05 17:28:05 -0400

    Update volume command to match new play changes

--- commands/volume.ts
+++ commands/volume.ts
@@ -8,16 +8,15 @@ const info = new SlashCommandBuilder()
     .setContexts([InteractionContextType.Guild])
     .addIntegerOption(option =>
         option.setName("volume")
-            .setDescription("Enter the volume integer (1 - 100) [Default: 10]")
+            .setDescription("Enter the desired volume (1 - 100) [Default: 30]")
             .setRequired(true))
 
 async function invoke(interaction: ChatInputCommandInteraction) {
     await interaction.deferReply({ flags: MessageFlags.Ephemeral })
-    const volume: number = interaction.options.getInteger("volume")!
     
     const voiceConnection: VoiceConnection | undefined = getVoiceConnection(interaction.guild!.id)
     if (voiceConnection && voiceConnection.joinConfig.channelId === (interaction.member as GuildMember).voice.channelId && globals.player[interaction.guild!.id].status === 1) {
-        globals.player[interaction.guild!.id].volume = volume / 100
+        globals.player[interaction.guild!.id].volume = interaction.options.getInteger("volume")! / 100
         globals.player[interaction.guild!.id].resource!.volume!.setVolume(globals.player[interaction.guild!.id].volume)
     }
 
